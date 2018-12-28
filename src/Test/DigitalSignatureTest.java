65d9e9e4261144b3ea98ea16787a319cbd8d2847
ar98nau <anr3@alumnes.udl.cat>
2018-12-28 10:37:08 +0100

    Test Signature

---
+++ src/Test/DigitalSignatureTest.java
@@ -0,0 +1,15 @@
+package Test;
+import Exception.WrongInputException;
+import data.DigitalSignature;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DigitalSignatureTest {
+
+    @Test
+    void exceptionTesting() {
+        assertThrows(WrongInputException.class, () -> new DigitalSignature(null));
+    }
+
+}
